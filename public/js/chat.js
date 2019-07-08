622fd833a8f926a7e4c7a7806751fe1d3f5c3d3f
Eduardo Iglesias <eriglesias@ufl.edu>
2019-07-08 15:44:08 -0400

    added functionality to allow clients and servers to listen to and emit events with data

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-io()
+const socket = io()
+socket.on('message', (message) => {
+  console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (event) => {
+  event.preventDefault()
+  const message = event.target.elements.message.value //grabs the element on the form with the "name" attribute equal to "message"
+  socket.emit('sendMessage', message)
+})
