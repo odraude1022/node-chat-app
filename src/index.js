99e8ad7b7d9813e52d8cc7b4e1399f6d1ae8eead
Eduardo Iglesias <eriglesias@ufl.edu>
2019-07-08 15:54:37 -0400

    add message for when a new user joins or a user disconnects

--- src/index.js
+++ src/index.js
@@ -17,9 +17,16 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
   console.log('New websocket connection')
   socket.emit('message', 'Welcome!')
+  socket.broadcast.emit('message', 'A new user has joined!')
+
   socket.on('sendMessage', (message) => {
     io.emit('message', message)
   })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left')
+  })
+  
 })
 
 server.listen(port, () => {
